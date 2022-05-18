83e40ad4e9d96d29c1594e8436eba399094d8ef0
ginagig123 <ginagigo123@gmail.com>
2022-05-19 00:20:10 +0800

    finish register with email

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -18,6 +18,25 @@ module Labook
           routing.halt '403', { message: 'Invalid credentials' }.to_json
         end
       end
+
+      routing.is 'register' do
+        # POST /api/v1/auth/register
+        routing.post do
+          reg_data = JsonRequestBody.parse_symbolize(request.body.read)
+          puts "reg: #{reg_data}"
+          VerifyRegistration.new(reg_data).call
+
+          response.status = 202
+          { message: 'Verification email sent'}.to_json
+        rescue VerifyRegistration::InvalidRegistration => e
+          routing.halt 400, {message: e.message }.to_json
+        rescue VerifyRegistration::EmailProviderError
+          routing.halt 500, { message: 'Error sending email' }.to_json
+        rescue StandardError => e
+          Api.logger.error "Could not verify registration: #{e.inspect}"
+          routing.halt 500
+        end
+      end
     end
   end
 end
