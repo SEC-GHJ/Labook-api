912e173b4e5c3af0ced2c4a46c5833af68632306
June Hsu <junehsu920179@gmail.com>
2022-06-14 10:52:31 +0800

    add fetch lab

--- app/db/migrations/003_department_create.rb
+++ app/db/migrations/003_department_create.rb
@@ -6,7 +6,7 @@ Sequel.migration do
   change do
     create_table(:departments) do
       foreign_key :school_name, :schools, type: :String
-      
+
       String :department_name, null: false
 
       primary_key [:school_name, :department_name]
